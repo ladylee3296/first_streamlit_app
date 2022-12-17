d5c6b7c8271f8b23965d6cea4430683525979df4
Lady Lee Ojagan <120694274+ladylee3296@users.noreply.github.com>
2022-12-17 18:30:59 +0800

    Update streamlit_app.py
    added an entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 import pandas
-import requests
 
 streamlit.title('My Moms New Healthy Diner')
 
@@ -22,11 +21,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+////////////////////////////////////////
+import request
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
