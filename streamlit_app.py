63a622608c31142a18a47d5939fff303a987a087
Lady Lee Ojagan <120694274+ladylee3296@users.noreply.github.com>
2022-12-17 18:32:51 +0800

    Update streamlit_app.py
    update typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import request
 
 streamlit.title('My Moms New Healthy Diner')
 
@@ -24,10 +25,9 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-import request
-
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
 streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
