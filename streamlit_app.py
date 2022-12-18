1a155da1d24c9d21fdc01699568f14d8d7cb4087
Lady Lee Ojagan <120694274+ladylee3296@users.noreply.github.com>
2022-12-18 11:09:32 +0800

    Update streamlit_app.py
    update typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,12 +35,12 @@ def get_fruityvice_data(this_fruit_choice):
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  streamlit.error("Please select a fruit to get information.")
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
 
-esle:
-  back_from_function = get_fruityvice_data(fruit_choice)
-  streamlit.dataframe(back_from_function)
+  esle:
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 
 except UrlError as e:
   streamlit.error()
